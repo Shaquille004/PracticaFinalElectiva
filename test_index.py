d5c32f3cb8910b7e71e52f2e25916e45b2284316
TuNombreDeUsuario <you@example.com>
2024-12-06 12:08:34 -0400

    tercer commit

--- test_index.py
+++ test_index.py
@@ -1,11 +1,16 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 class TestHelloWorld(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=options)
 
     def test_title(self):
         self.driver.get("http://localhost:8000")
@@ -21,3 +26,4 @@ class TestHelloWorld(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
